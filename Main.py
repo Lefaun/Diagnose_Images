767fe0a8e46581e7f868622e9f2435208ddc3a0b
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-05-03 15:48:50 +0100

    Main.py

--- Main.py
+++ Main.py
@@ -32,7 +32,8 @@ data_transforms = {
     ]),
 }
 st.title("Sistema de Classificação Diagnóstico")
-data_dir = 'https://drive.google.com/drive/folders/1O6ZimdPrpby5opBy8WtavAr6X7Hp2xdU?usp=sharing'
+data_dir = 'https://drive.google.com/drive/folders/1O6ZimdPrpby5opBy8WtavAr6X7Hp2xdU?usp=sharing''
+gdown.download(data_dir, quiet=False)
 
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
                                           data_transforms[x])
