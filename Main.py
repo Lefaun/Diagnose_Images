e3768340ac248759be5216239959ae1632b4693b
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-05-04 12:59:55 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -73,7 +73,7 @@ def imshow(inp, title=None):
     std = np.array([0.229, 0.224, 0.225])
     inp = std * inp + mean
     inp = np.clip(inp, 0, 1)
-    st.pyplot(imshow(inp))
+    st.pyplot(inp)
     if title is not None:
         st.pyplot(title(title))
     plt.pause(0.001)  # pause a bit so that plots are updated
