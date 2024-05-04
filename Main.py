2ce5b0c6d029d09fd5f46f8cf2843d2378e6785a
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-05-04 12:44:55 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -226,8 +226,8 @@ exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
 
 visualize_model(model_conv)
 
-plt.ioff()
-plt.show()
+st.pyplot.ioff()
+st.pyplot.show()
 
 def visualize_model_predictions(model,img_path):
     was_training = model.training
@@ -259,5 +259,6 @@ with col1:
 
 with col2:
     st.image(uploaded_file)
-    plt.ioff()
-    plt.show()
+    
+pst.pyplot.ioff()
+st.pyplot.show()
