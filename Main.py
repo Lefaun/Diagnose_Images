9ec9fe44843304fc6cb46ee4013a79b0f4d54cdd
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-05-04 11:23:11 +0100

    Main.py

--- Main.py
+++ Main.py
@@ -42,7 +42,7 @@ data_transforms = {
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.connection('gcsfs', type=FilesConnection)
-data_dir = conn.read("Diagnose-Images/chest_xray", input_format="jpeg", ttl=600)
+data_dir = conn.read("Diagnose-Images/chest_xray", input_format="jpg", ttl=600)
 
 # Print results.
 for row in df.itertuples():
