04c2ccab56ea43f838c65dd0fcabd91d7b2fca4a
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-05-04 13:58:56 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -88,7 +88,7 @@ st.header("Imagens Classificadas")
 out = torchvision.utils.make_grid(inputs)
 
 imshow(out, title=[class_names[x] for x in classes])
-st.pyplot(inputs, classes)
+st.pyplot(inputs)
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
